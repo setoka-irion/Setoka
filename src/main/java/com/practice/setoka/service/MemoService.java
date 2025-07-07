5e6f2b66bde51f1b994caca1726663b2453e9f20
park222222 <atkdwnd123@naver.com>
2025-07-07 17:55:47 +0900

    메모 서비스까지

---
+++ src/main/java/com/practice/setoka/service/MemoService.java
@@ -0,0 +1,46 @@
+package com.practice.setoka.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.practice.setoka.dao.Memo;
+import com.practice.setoka.dto.MemoDto;
+import com.practice.setoka.mapper.MemoMapper;
+
+@Service
+public class MemoService {
+
+	@Autowired
+	private MemoMapper memoMapper;
+	
+	public List<Memo> memoSelectAll() {
+		return memoMapper.memoSelectAll();
+	}
+	
+	public Memo memoSelectByNum(int num) {
+		return memoMapper.memoSelectByNum(num);
+	}
+	
+	public List<Memo> memoSelectByUserNum(int userNum) {
+		return memoMapper.memoSelectByUserNum(userNum);
+	}
+	
+	public List<Memo> memoSelectByAnimalNum(int animalNum) {
+		return memoMapper.memoSelectByAnimalNum(animalNum);
+	}
+	
+	public int insertMemo(MemoDto memoDto) {
+		return memoMapper.insertMemo(memoDto);
+	}
+	
+	public int updateMemo(int num, MemoDto memoDto) {
+		return memoMapper.updateMemo(num, memoDto);
+	}
+	
+	public int deleteMemo(int num) {
+		return memoMapper.deleteMemo(num);
+	}
+}
+
