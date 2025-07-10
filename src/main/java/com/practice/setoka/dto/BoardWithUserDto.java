2df913bcf2b915ba7f9d57549452c12dc9c9ea04
tco5555 <tco5555@naver.com>
2025-07-10 15:41:09 +0900

    매핑중

---
+++ src/main/java/com/practice/setoka/dto/BoardWithUserDto.java
@@ -0,0 +1,108 @@
+package com.practice.setoka.dto;
+
+import java.time.LocalDateTime;
+
+public class BoardWithUserDto {
+
+	private int num;
+	private int usernum;
+	private String id;
+	private String nickname;
+	private String realname;
+	private String grade;
+	private String title;
+	private String content;
+	private byte type;
+	private int likes;
+	private int views;
+	private int price;
+	private String area;
+	private LocalDateTime registerDate;
+	
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getUsernum() {
+		return usernum;
+	}
+	public void setUsernum(int usernum) {
+		this.usernum = usernum;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getRealname() {
+		return realname;
+	}
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public byte getType() {
+		return type;
+	}
+	public void setType(byte type) {
+		this.type = type;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	public int getViews() {
+		return views;
+	}
+	public void setViews(int views) {
+		this.views = views;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public LocalDateTime getRegisterDate() {
+		return registerDate;
+	}
+	public void setRegisterDate(LocalDateTime registerDate) {
+		this.registerDate = registerDate;
+	}
+
+}
