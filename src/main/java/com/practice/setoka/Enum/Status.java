8242f0a75a6df9e4937092a839b04cb1bd94e717
biueapple <duqorghg@gmail.com>
2025-07-14 13:20:59 +0900

    asdf

--- src/main/java/com/practice/setoka/Enum/Status.java
+++ src/main/java/com/practice/setoka/Enum/Status.java
@@ -1,6 +1,19 @@
 package com.practice.setoka.Enum;
 
 public enum Status {
-	기본,
-	삭제
+	정상,
+	삭제;
+	
+	public String Get(int index)
+	{
+		switch(index)
+		{
+		case 0:
+			return "정상";
+		case 1:
+			return "삭제";
+		}
+		
+		return "";
+	}
 }
