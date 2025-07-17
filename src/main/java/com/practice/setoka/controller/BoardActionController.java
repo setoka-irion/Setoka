a3a0b9055fdb8b287e21c45fbe290c2235b5415a
tco5555 <tco5555@naver.com>
2025-07-17 14:33:29 +0900

    입양페이지

--- src/main/java/com/practice/setoka/controller/BoardActionController.java
+++ src/main/java/com/practice/setoka/controller/BoardActionController.java
@@ -278,6 +278,7 @@ public class BoardActionController {
 		    if(authUser != null) {
 		        boardService.increaseLikesBoard(num);
 		    }
+		    
 		    return "redirect:/AdoptDetail?num=" + num;
 		}
 	}
