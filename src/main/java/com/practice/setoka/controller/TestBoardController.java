1188a6dc923025d1171b2492da667bf6a50309d0
CYJ0217 <angela5243@naver.com>
2025-07-22 09:42:07 +0900

    그냥 테스트 코드

--- src/main/java/com/practice/setoka/controller/TestBoardController.java
+++ src/main/java/com/practice/setoka/controller/TestBoardController.java
@@ -3,27 +3,32 @@ package com.practice.setoka.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.practice.setoka.JsonFileWriter;
+import com.practice.setoka.service.TestService;
 
 @Controller
 public class TestBoardController 
 {
-	
+	@Autowired
+	TestService testservice;
 	// 게시판
-	@GetMapping(value = "TestBoard")
+	@GetMapping(value = "testboard")
 	public String board(Model model)
 	{
 		Map<String, Object> mapData = new HashMap<>();
-		mapData.put("test", "value");
+		mapData.put("제목", "어린왕자");
+		mapData.put("내용", "왕자이야기");
 		JsonFileWriter.saveJsonToFile(mapData, "C:/images/test.json");
 		
 		try
 		{
-			mapData = JsonFileWriter.readJsonFileToMap("C:/images/test.json");
+			testservice.insertContent("C:/images/test.json");
+			mapData = JsonFileWriter.readJsonFileToMap(testservice.selectContent(4));
 			mapData.forEach((key, value) -> {
 			    System.out.println("key = " + key + ", value = " + value);
 			});
@@ -33,7 +38,8 @@ public class TestBoardController
 			e.printStackTrace();
 		}
 		
-		
 		return "TestBoard";
 	}
+	
+	
 }
