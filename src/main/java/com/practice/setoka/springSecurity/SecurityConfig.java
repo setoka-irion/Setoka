e936d248942f2075df51965a7527e319bd482edd
LeeGwangSik <jine9220@naver.com>
2025-07-23 16:15:57 +0900

    헤더 추가

--- src/main/java/com/practice/setoka/springSecurity/SecurityConfig.java
+++ src/main/java/com/practice/setoka/springSecurity/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
 		http
 			.csrf(csrf -> csrf.disable())
 			.authorizeHttpRequests(auth -> auth
-			    .requestMatchers("/", "/Login", "/SignUp", "/Adopt", "/sendSingUpCode").permitAll()
+			    .requestMatchers("/", "/Login", "/SignUp", "/Adopt", "/sendSingUpCode", "/loginUser").permitAll()
 			    //.requestMatchers("/AllUsers").hasRole("관리자")
 			    .anyRequest().authenticated()
 			)
