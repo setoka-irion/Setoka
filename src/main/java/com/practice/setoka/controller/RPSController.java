2ddba8943866c7a9e44cbdfd82a7ab9908b22874
biueapple <duqorghg@gmail.com>
2025-07-23 11:12:41 +0900

    db interlock

--- src/main/java/com/practice/setoka/controller/RPSController.java
+++ src/main/java/com/practice/setoka/controller/RPSController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.practice.setoka.Redirect;
+import com.practice.setoka.Enum.GameResult;
 import com.practice.setoka.dao.Users;
 import com.practice.setoka.service.RPSService;
 import com.practice.setoka.springSecurity.CustomUserDetails;
@@ -23,9 +23,6 @@ public class RPSController {
 	@GetMapping(value = "RPS")
 	public String RPSForm(Model model, @AuthenticationPrincipal CustomUserDetails authUser)
 	{
-		if(authUser == null)
-			return Redirect.home;
-		
 		Users user = authUser.getUser();
 		model.addAttribute("user", user);
 		return "RPS";
@@ -35,14 +32,10 @@ public class RPSController {
 	public String ResultRPS(@RequestParam("choice") String choice, @RequestParam("point") int point,
 			@AuthenticationPrincipal CustomUserDetails authUser, RedirectAttributes redirectAttributes)
 	{
-		if(authUser == null)
-			return Redirect.home;
 		Users user = authUser.getUser();
-		if(user == null)
-			return Redirect.home;
 		
-		String result = rpsService.Play(choice, point, user);
-		redirectAttributes.addFlashAttribute("result", result);
+		GameResult result = rpsService.Play(choice, point, user);
+		redirectAttributes.addFlashAttribute("result", result.getDisplayName());
 		return "redirect:/RPS";
 	}
 }
--- src/main/java/com/practice/setoka/service/RPSService.java
+++ src/main/java/com/practice/setoka/service/RPSService.java
@@ -5,55 +5,61 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.practice.setoka.Enum.GameResult;
+import com.practice.setoka.Enum.RPSEnum;
 import com.practice.setoka.dao.Users;
-import com.practice.setoka.dto.UsersDto;
+import com.practice.setoka.mapper.RPSMapper;
 
 @Service
 public class RPSService {
 	//0 패배 1 승리 2 무승부
-	int[][] result = new int[][] {
-			{2, 0, 1},
-			{1, 2, 0},
-			{0, 1, 2}	
+	GameResult[][] result = new GameResult[][] {
+			{GameResult.DRAW, GameResult.LOSE, GameResult.WIN},
+			{GameResult.WIN, GameResult.DRAW, GameResult.LOSE},
+			{GameResult.LOSE, GameResult.WIN, GameResult.DRAW}	
 	};
 	
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private RPSMapper rpsMapper;
 	
-	public String Play(String choice, int point, Users user)
+	public GameResult Play(String choice, int point, Users user)
 	{
 		Random random = new Random();
 		int ran = random.nextInt(3);
 		int ch = 0;
 		if(choice.equals("SCISSORS"))
+		{
 			ch = 0;
+		}
 		else if(choice.equals("ROCK"))
+		{
 			ch = 1;
+		}
 		else if(choice.equals("PAPER"))
+		{
 			ch = 2;
+		}
 		
-		int re = result[ch][ran];
-		String s = "";
+		GameResult re = result[ch][ran];
 		//유저 포인트 증가, 감소
 		switch(re)
 		{
-		case 0:	//패배
-			user.setPoint(user.getPoint() - point);
-			System.out.println("패배");
-			s = "패배";
+		case LOSE:	//패배
+			point *= -1;
 			break;
-		case 1:	//승리
-			user.setPoint(user.getPoint() + point);
-			System.out.println("승리");
-			s = "승리";
+		case WIN:	//승리
 			break;
-		case 2:	//무승부
-			System.out.println("무승부");
-			s = "무승부";
+		case DRAW:	//무승부
+			point = 0;
 			break;
 		}
-		System.out.println(user.getPoint());
+		
+		user.setPoint(user.getPoint() + point);
+
+		rpsMapper.insertRPSResult(user.getNum(), RPSEnum.getEnum(ch), RPSEnum.getEnum(ran), re, point);
 		userService.userPointUpdate(user.getId(), user.getPoint());
-		return s;
+		return re;
 	}
 }
