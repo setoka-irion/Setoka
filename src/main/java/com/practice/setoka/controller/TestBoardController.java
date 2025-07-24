edb75a55c26133027177997309cd94e67132d0ed
tco5555 <tco5555@naver.com>
2025-07-24 17:05:18 +0900

    json코드 수정으로 test주석처리

--- src/main/java/com/practice/setoka/controller/TestBoardController.java
+++ src/main/java/com/practice/setoka/controller/TestBoardController.java
@@ -1,45 +1,45 @@
-package com.practice.setoka.controller;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.practice.setoka.JsonFileWriter;
-import com.practice.setoka.service.TestService;
-
-@Controller
-public class TestBoardController 
-{
-	@Autowired
-	TestService testservice;
-	// 게시판
-	@GetMapping(value = "testboard")
-	public String board(Model model)
-	{
-		Map<String, Object> mapData = new HashMap<>();
-		mapData.put("제목", "어린왕자");
-		mapData.put("내용", "왕자이야기");
-		JsonFileWriter.saveJsonToFile(mapData, "C:/images/test.json");
-		
-		try
-		{
-			testservice.insertContent("C:/images/test.json");
-			mapData = JsonFileWriter.readJsonFileToMap(testservice.selectContent(4));
-			mapData.forEach((key, value) -> {
-			    System.out.println("key = " + key + ", value = " + value);
-			});
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		
-		return "TestBoard";
-	}
-	
-	
-}
+//package com.practice.setoka.controller;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//import com.practice.setoka.JsonFileWriter;
+//import com.practice.setoka.service.TestService;
+//
+//@Controller
+//public class TestBoardController 
+//{
+//	@Autowired
+//	TestService testservice;
+//	// 게시판
+//	@GetMapping(value = "testboard")
+//	public String board(Model model)
+//	{
+//		Map<String, Object> mapData = new HashMap<>();
+//		mapData.put("제목", "어린왕자");
+//		mapData.put("내용", "왕자이야기");
+//		JsonFileWriter.saveJsonToFile(mapData, "C:/images/test.json");
+//		
+//		try
+//		{
+//			testservice.insertContent("C:/images/test.json");
+//			mapData = JsonFileWriter.readJsonFileToMap(testservice.selectContent(4));
+//			mapData.forEach((key, value) -> {
+//			    System.out.println("key = " + key + ", value = " + value);
+//			});
+//		}
+//		catch(Exception e)
+//		{
+//			e.printStackTrace();
+//		}
+//		
+//		return "TestBoard";
+//	}
+//	
+//	
+//}
