6339b7f107a23ed50c230691127d342f98890ed5
tco5555 <tco5555@naver.com>
2025-07-25 16:57:37 +0900

    중고장터까지

--- src/main/java/com/practice/setoka/controller/BoardActionController.java
+++ src/main/java/com/practice/setoka/controller/BoardActionController.java
@@ -637,7 +637,7 @@ public class BoardActionController {
 				 redirectAttributes.addFlashAttribute("errorMessage", "작성자만 삭제가능!"); 
 				 return "redirect:/WalkTrail";
 			}
-
+			System.out.println("삭제진행");
 			boardService.deleteBoard(num);
 
 			 return "redirect:/WalkTrail";
@@ -804,5 +804,372 @@ public class BoardActionController {
 
 			return "redirect:/WalkTrailDetail/" + boardNum;
 		}
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+		// 중고장터 메인페이지
+				@GetMapping(value = "/UsedGoods")
+				public String usedGoodsMain(Model model, @RequestParam(value = "keyword", required = false) String keyword,
+						@RequestParam(value = "field", required = false) String field) {
+
+					// 중고장터 게시판 총 게시글 수
+					int countBoards = boardService.countBoards(3);
+					model.addAttribute("countBoards", countBoards);
+
+					// 중고장터 게시판 내부 검색
+					List<BoardWithUserDto> searchResult;
+					if (keyword == null || keyword.isEmpty()) {
+						// 검색 값 안넣었을 경우 전체 게시판 리스트 출력
+						searchResult = boardService.findBoardsByType(3);
+					} else {
+						switch (field) {
+						case "title":
+							searchResult = boardService.findBoardsByTitle(keyword.trim());
+							break;
+						case "content":
+							searchResult = boardService.findBoardsByContent(keyword.trim());
+							break;
+						case "nickname":
+							searchResult = boardService.findBoardsByUserId(keyword.trim());
+							break;
+						default:
+							searchResult = boardService.searchAll(keyword.trim());
+						}
+					}
+					for (var d : searchResult) {
+						System.out.println(d.getBoardNum());
+					}
+					// 메인 리스트 출력
+					model.addAttribute("mainList", searchResult);
+					// 검색값 유지 (검색창 value 유지용)
+					model.addAttribute("keyword", keyword);
+					model.addAttribute("field", field);
+
+					return "Board/UsedGoods";
+				}
+
+				// 중고장터 상세 페이지 (조회수증가)
+				@GetMapping(value = "/UsedGoodsDetail/{num}")
+				public String usedGoodsDetail(@PathVariable("num") int num,
+						@RequestParam(value = "editCommentNum", required = false) Integer editCommentNum,
+						@AuthenticationPrincipal CustomUserDetails authUser, HttpSession session, Model model) {
+
+					// 상세 내용 보여줌
+					BoardWithUserDto Detail = boardService.findBoardByNum(num);
+					model.addAttribute("detail", Detail);
+					System.out.println("detail null? → " + (Detail == null));
+
+					// 세션에서 조회한 게시글 번호 리스트 받아오기, 없으면 만듦(조회수증가기능)
+					@SuppressWarnings("unchecked")
+					List<Integer> viewedBoards = (List<Integer>) session.getAttribute("VIEWED_BOARDS");
+					if (viewedBoards == null) {
+						viewedBoards = new ArrayList<>();
+					}
+
+					// 유저에 한해 조회수 증가
+					if (authUser != null) { // 원래 authUser는 비어있기 때문에 아래의 코드가 있으면 무조건 로그인 시킴.
+						Users user = (Users) authUser.getUser();
+						if (user != null) {
+							// 이 게시글 번호가 세션에 없으면 조회수 증가 및 세션에 저장
+							if (!viewedBoards.contains(num)) {
+								boardService.increaseViewsBoard(num);
+								viewedBoards.add(num);
+								session.setAttribute("VIEWED_BOARDS", viewedBoards);
+							}
+						}
+					}
+					// 해당 게시글 기존 댓글 보여주기
+					List<CommentInfoDto> comments = commentsService.findCommentsByBoardNum(num);
+					model.addAttribute("comments", comments);
+
+					// 댓글 수정 기능
+					if (editCommentNum != null) {
+						CommentInfoDto commentToEdit = commentsService.findCommentByNum(editCommentNum);
+						model.addAttribute("commentToEdit", commentToEdit);
+					}
+
+					System.out.println("Detail 진입");
+					return "Board/UsedGoodsDetail";
+				}
+
+				// 중고장터 게시글 등록
+				@GetMapping(value = "/UsedGoodsRegist")
+				public String usedGoodsRegistForm(@AuthenticationPrincipal CustomUserDetails authUser, Model model) {
+					// 로그인 검증
+					Users user = (Users) authUser.getUser();
+					// 세션의 닉네임 저장
+					model.addAttribute("writerNick", user.getNickName());
+
+					// 글 등록시 유저번호 저장
+					BoardDto boardDto = new BoardDto();
+					boardDto.setUserNum(user.getNum());
+					boardDto.setType(3);
+					model.addAttribute("boardDto", boardDto);
+					return "Board/UsedGoodsRegist";
+				}
+
+				// 중고장터 게시글 등록
+				@PostMapping(value = "/UsedGoodsRegist")
+				public String usedGoodsRegistSubmit(
+						// 오류 검증
+						@Valid BoardDto boardDto,
+						@RequestParam("images") List<MultipartFile> images,
+						BindingResult bindingResult, Model model) {
+
+					if (bindingResult.hasErrors()) {
+						return "Board/UsedGoodsRegist";
+					}
+					
+					Map<String, Object> apple = new HashMap<String, Object>();
+					apple.put("text",boardDto.getContent());
+					String str = JsonFileWriter.saveJsonToFile(apple, "C:/images/", "test");
+					boardDto.setContent(str);
+					boardService.insertBoard(boardDto);
+					return "redirect:/UsedGoods";
+				}
+
+				// 중고장터 게시글 수정
+				@GetMapping(value = "/UsedGoodsUpdate/{num}")
+				public String usedGoodsUpdateForm(@PathVariable("num") int num, Model model,
+						@AuthenticationPrincipal CustomUserDetails authUser, 
+						RedirectAttributes redirectAttributes) {
+					// 작성자 정보 가져오기(작성자, 관라자 삭제 권한 확인용)
+							BoardWithUserDto board = boardService.findBoardByNum(num);
+
+							// 유저 정보 가져오기
+							Users user = (Users) authUser.getUser();
+
+							// 작성자, 관리자 삭제권한 부여
+							boolean isAuthur = user.getNum() == board.getUserNum();
+							boolean isAdmin = "관리자".equals(user.getGrade());
+
+							if (!isAuthur && !isAdmin) {
+								 redirectAttributes.addFlashAttribute("errorMessage", "작성자만 수정가능!"); 
+								 return "redirect:/UsedGoodsDetail/" + num;
+							}
+					// 해당 게시글 내용
+					// 상세보기와 동일 코드
+					model.addAttribute("board", board); // 수정 폼에서 기본값으로 사용
+
+					return "Board/UsedGoodsUpdate";
+				}
+
+				// 중고장터 게시글 수정
+				@PostMapping(value = "/UsedGoodsUpdate/{num}")
+				public String usedGoodsUpdateSubmit(@PathVariable("num") int num, Model model, @Valid Board board,
+						BindingResult bindingResult, @AuthenticationPrincipal CustomUserDetails authUser,
+						RedirectAttributes redirectAttributes) {
+					redirectAttributes.addAttribute("num", num);
+
+					// 유저 검증
+					Users user = (Users) authUser.getUser();
+				    BoardWithUserDto existing = boardService.findBoardByNum(num);
+
+				    boolean isAuthor = user.getNum() == existing.getUserNum();
+				    boolean isAdmin = "관리자".equals(user.getGrade());
+
+				    if (!isAuthor && !isAdmin) {
+				        redirectAttributes.addFlashAttribute("errorMessage", "권한이 없습니다.");
+				        return "redirect:/UsedGoodsDetail" + num;
+				    }
+				    
+					// 오류 발생시 다시 수정페이지로
+					if (bindingResult.hasErrors()) {
+						model.addAttribute("board", board); // 반드시 넣어줘야 함
+						System.out.println("45634");
+						return "/Board/UsedGoodsUpdate";
+					}
+					System.out.println("123");
+					board.setNum(num);
+
+					boardService.updateBoard(board);
+					return "redirect:/UsedGoodsDetail/" + num;
+				}
+
+				// 삭제
+				@PostMapping("/UsedGoodsDelete/{num}")
+				public String usedGoodsDelete(@PathVariable("num") int num, @AuthenticationPrincipal CustomUserDetails authUser,
+						RedirectAttributes redirectAttributes) {
+
+					// 작성자 정보 가져오기(작성자, 관라자 삭제 권한 확인용)
+					BoardWithUserDto board = boardService.findBoardByNum(num);
+
+					// 유저 정보 가져오기
+					Users user = (Users) authUser.getUser();
+
+					// 작성자, 관리자 삭제권한 부여
+					boolean isAuthur = user.getNum() == board.getUserNum();
+					boolean isAdmin = "관리자".equals(user.getGrade());
+
+					if (!isAuthur && !isAdmin) {
+						 redirectAttributes.addFlashAttribute("errorMessage", "작성자만 삭제가능!"); 
+						 return "redirect:/WalkTrail";
+					}
+					System.out.println("삭제진행");
+					boardService.deleteBoard(num);
+
+					 return "redirect:/UsedGoods";
+				}
+
+				
+				
+				
+				
+				// 신고기능
+				@PostMapping("/UsedGoodsDetail/{num}/report")
+				public String usedGoodsReportBoard(@PathVariable("num") int num, @AuthenticationPrincipal CustomUserDetails authUser) {
+					
+					Users user = authUser.getUser();
+					if (user != null)
+					{	//로그인유저가 신고시 내역없으면 신고가능
+						if(boardService.findReportByBC(num, user.getNum()) == null)
+						{
+							boardService.reportBoard(num, user.getNum());
+						}
+					}
+					//중복신고 안됌
+					return "redirect:/UsedGoodsDetail/" + num;
+				}
+
+				//댓글 신고
+				@PostMapping("/UsedGoodsDetail/{num}/report/comment")
+				public String usedGoodsReportComment(@PathVariable("num") int num, @AuthenticationPrincipal CustomUserDetails authUser) {
+					
+					Users user = authUser.getUser();
+					if (user != null)
+					{
+						if(commentsService.findReportByCB(num, user.getNum()) == null)
+						{
+							commentsService.reportComment(num, user.getNum());
+						}
+					}
+					
+					return "redirect:/UsedGoodsDetail/" + num;
+				}
+				
+				
+				
+				
+				
+				
+				// 게시글 좋아요
+				@PostMapping("/UsedGoodsDetail/{num}/like")
+				public String usedGoodsLikeBoard(@PathVariable("num") int num, @AuthenticationPrincipal CustomUserDetails authUser) {
+
+					if (authUser.getUser() != null)
+						likeService.likeBoard(new LikeDto(authUser.getUser().getNum(), num));
+
+					return "redirect:/UsedGoodsDetail/" + num;
+				}
+
+				
+				
+				
+				
+				// 댓글 등록
+				@PostMapping(value = "UsedGoodsDetail/{num}/comment")
+				public String usedGoodsAddComment(@PathVariable("num") int boardNum, // 게시글 넘버
+						@RequestParam("content") String content, // 댓글내용
+						@RequestParam(value = "parentNum", defaultValue = "0") int parentNum, // 대댓글 기능 없어도 됌
+						@AuthenticationPrincipal CustomUserDetails authUser, // 로그인 검증용
+						Model model) {
+
+					// 로그인 유저 정보 가져오기용
+					Users user = (Users) authUser.getUser();
+
+					// 댓글 불러오기
+					List<CommentInfoDto> comments = commentsService.findCommentsByBoardNum(boardNum);
+
+					model.addAttribute("comments", comments);
+
+					// 댓글 작성
+					CommentInfoDto commentInfoDto = new CommentInfoDto();
+					commentInfoDto.setUserNum(user.getNum());
+					commentInfoDto.setBoardNum(boardNum);
+					commentInfoDto.setContent(content);
+					commentInfoDto.setParentNum(parentNum); // 대댓글기능이라 없어도 됌
+					commentsService.insertComment(commentInfoDto);
+
+					return "redirect:/UsedGoodsDetail/" + boardNum;
+				}
+
+				// 댓글 수정
+				@PostMapping("/UsedGoodsDetail/{num}/comment/update")
+				public String usedGoodsEditComment(@PathVariable("num") int boardNum, @RequestParam("commentNum") int commentNum,
+						@RequestParam("content") String content, @AuthenticationPrincipal CustomUserDetails authUser,
+						RedirectAttributes redirectAttributes) {
+
+					// 로그인 확인
+					Users user = (Users) authUser.getUser();
+					if (user == null) {
+						redirectAttributes.addFlashAttribute("errorMessage", "로그인이 필요합니다.");
+						return "redirect:/UsedGoodsDetail/" + boardNum;
+					}
+
+					// 본인확인
+					CommentInfoDto originalComment = commentsService.findCommentByNum(commentNum);
+					if (originalComment == null || originalComment.getUserNum() != user.getNum()) {
+						redirectAttributes.addFlashAttribute("errorMessage", "본인 댓글만 수정할 수 있습니다.");
+						return "redirect:/UsedGoodsDetail/" + boardNum;
+					}
+
+					// 댓글 내용불러오기
+					CommentInfoDto commentInfoDto = new CommentInfoDto();
+					commentInfoDto.setNum(commentNum);
+					commentInfoDto.setContent(content);
+					commentInfoDto.setUserNum(user.getNum());
+
+					commentsService.updateComment(commentInfoDto);
+
+					// html 에러메세지 보내기
+					redirectAttributes.addFlashAttribute("successMessage", "수정 완료!");
+					return "redirect:/UsedGoodsDetail/" + boardNum;
+				}
+
+				// 댓글 삭제
+				@PostMapping("/UsedGoodsDetail/{num}/comment/delete")
+				public String usedGoodsDeleteComment(@AuthenticationPrincipal CustomUserDetails authUser, @PathVariable("num") int boardNum,
+						@RequestParam("commentNum") int commentNum,
+						RedirectAttributes redirectAttributes) {
+
+					// 현재 로그인한 유저 정보
+					Users loginUser = authUser.getUser();
+
+					// 댓글 정보 조회 (작성자 확인용)
+					CommentInfoDto comment = commentsService.findCommentByNum(commentNum);
+
+					// 작성자 본인인지 확인
+					if (comment.getUserNum() != loginUser.getNum()) {
+						// 본인이 아니라면 삭제하지 않고 되돌림 (또는 에러 페이지로)
+						redirectAttributes.addFlashAttribute("errorMessage","작성자만 삭제 할 수 있다는 거임!");
+						return "redirect:/UsedGoodsDetail/" + boardNum ;
+					}
+
+					commentsService.deleteComment(commentNum);
+					redirectAttributes.addFlashAttribute("successMessage","삭제 완료!");
+					return "redirect:/UsedGoodsDetail/" + boardNum ;
+				}
+
+				// 댓글 좋아요
+				@PostMapping("/UsedGoodsDetail/{num}/comment/like")
+				public String usedGoodsLikeComment(@AuthenticationPrincipal CustomUserDetails authUser, @PathVariable("num") int boardNum,
+						@RequestParam("commentNum") int commentNum) {
+
+					// 로그인 유저 확인
+					Users loginUser = authUser.getUser();
+					if (loginUser == null) {
+						return "redirect:/UsedGoodsDetail/" + boardNum;
+					}
+
+					// 댓글 존재 확인
+					CommentInfoDto comment = commentsService.findCommentByNum(commentNum);
+					if (comment == null) {
+						return "redirect:/UsedGoodsDetail/" + boardNum;
+					}
+					System.out.println("댓글 좋아");
+					if (authUser.getUser() != null)
+						commentLikeService.likeComment(new CommentLikeDto(loginUser.getNum(), commentNum));
+
+					return "redirect:/UsedGoodsDetail/" + boardNum;
+				}
 	
 }
