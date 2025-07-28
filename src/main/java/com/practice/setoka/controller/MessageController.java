b661877a547a3f65e87a498cb6e37416e27f30ec
biueapple <duqorghg@gmail.com>
2025-07-28 17:21:20 +0900

    message send list

--- src/main/java/com/practice/setoka/controller/MessageController.java
+++ src/main/java/com/practice/setoka/controller/MessageController.java
@@ -8,32 +8,56 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.practice.setoka.Enum.Item;
 import com.practice.setoka.dao.Message;
 import com.practice.setoka.dto.MessageDto;
 import com.practice.setoka.service.MessageService;
+import com.practice.setoka.service.UserService;
 import com.practice.setoka.springSecurity.CustomUserDetails;
 
 @Controller
 public class MessageController {
 	@Autowired
 	private MessageService messageService;
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping(value = "CreateLetter")
-	public String CreateLetter(Model model)
+	public String CreateLetter(Model model, @AuthenticationPrincipal CustomUserDetails authUser)
 	{
 		model.addAttribute("itemTypes", Item.values());
+		model.addAttribute("max", authUser.getUser().getPoint());
+		System.out.println(authUser.getUser().getPoint());
 		return "CreateLetter";
 	}
 	
 	@PostMapping(value = "SendMessage")
 	public String sendMessage(MessageDto dto, @AuthenticationPrincipal CustomUserDetails authUser)
 	{
+		//포인트가 부족한 경우
+		if(authUser.getUser().getPoint() < dto.getItem_Value())
+			return "redirect:/CreateLetter";
+		
 		dto.setSender(authUser.getUser().getId());
+		
+		//아이템이 없는 경우
 		if(dto.getItem_Type() == null)
 			dto.setItem_Value(0);
-		messageService.sendMessage(dto);
+		
+		if(messageService.sendMessage(dto))
+		{
+			switch(dto.getItem_Type())
+			{
+			case POINT:
+				int newPoint = authUser.getUser().getPoint() - dto.getItem_Value();
+				userService.userPointUpdate(dto.getSender(), newPoint);
+				authUser.getUser().setPoint(newPoint);
+				break;
+			}
+		}
+		
 		return "redirect:/CreateLetter";
 	}
 	
@@ -41,8 +65,14 @@ public class MessageController {
 	public String messageList(Model model, @AuthenticationPrincipal CustomUserDetails authUser)
 	{
 		List<Message> list = messageService.receiverSelect(authUser.getUser().getId());
-		System.out.println(list.size());
 		model.addAttribute("messageList", list);
 		return "messageList";
+	}
+	
+	@GetMapping(value = "GettingGift")
+	public String GettingGift(@RequestParam("messageNum") int num, @AuthenticationPrincipal CustomUserDetails authUser)
+	{
+		messageService.GettingMessage(num);
+		return "redirect:/MessageList";
 	}
 }
--- src/main/java/com/practice/setoka/service/MessageService.java
+++ src/main/java/com/practice/setoka/service/MessageService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.practice.setoka.Enum.MessageStatus;
 import com.practice.setoka.dao.Message;
 import com.practice.setoka.dto.MessageDto;
 import com.practice.setoka.mapper.MessageMapper;
@@ -16,13 +17,13 @@ public class MessageService {
 	@Autowired
 	private UserService userService;
 	
-	public void sendMessage(MessageDto dto)
+	public boolean sendMessage(MessageDto dto)
 	{
 		if(userService.selectByID(dto.getReceiver()) == null)
-			return;
+			return false;
 		if(userService.selectByID(dto.getSender()) == null)
-			return;
-		messageMapper.insertMessage(dto);
+			return false;
+		return messageMapper.insertMessage(dto);
 	}
 	
 	public List<Message> sendSelect(String userId)
@@ -33,5 +34,26 @@ public class MessageService {
 	public List<Message> receiverSelect(String userId)
 	{
 		return messageMapper.selectReceiverMessage(userId);
+	}
+	
+	public void GettingMessage(int num)
+	{
+		Message message = messageMapper.selectToNum(num);
+		if(message.getItem_Type() == null)
+		{
+			messageMapper.updateMessageStatus(num, MessageStatus.TAKE);
+			return;
+		}
+		
+		switch(message.getItem_Type())
+		{
+		case POINT:
+			userService.userPointUpdate(message.getReceiver(), message.getItem_Value());
+			break;
+			default:
+				
+				break;
+		}
+		messageMapper.updateMessageStatus(num, MessageStatus.TAKE);
 	}
 }
