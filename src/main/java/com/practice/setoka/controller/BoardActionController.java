b10d6ed9d085c9e6bfc17951126fbb7b476e1cc3
tco5555 <tco5555@naver.com>
2025-08-12 15:07:20 +0900

    인기글 조회수 수정

--- src/main/java/com/practice/setoka/controller/BoardActionController.java
+++ src/main/java/com/practice/setoka/controller/BoardActionController.java
@@ -109,7 +109,6 @@ public class BoardActionController {
 
 		// 인기게시글
 		List<BoardWithUserDto> popularPosts = boardService.popularPosts(1);
-		System.out.println(viewType);
 		
 		// 뷰 타입 세션 저장 또는 불러오기
 		if (viewType != null && !viewType.isEmpty()) {
